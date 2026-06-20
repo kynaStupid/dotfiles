576d9a37e22bd144137fc70514ce8a49777668a6
skibidisigma122333 <testingscams122333@gmail.com>
2026-06-20 10:15:22 +0800

    waybar theme

--- flake.nix
+++ flake.nix
@@ -2,20 +2,25 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+	catppuccin.url = "github:catppuccin/nix";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
+  outputs = { nixpkgs, home-manager, catppuccin, ... }: {
     homeConfigurations.sheb =
       home-manager.lib.homeManagerConfiguration {
         pkgs = import nixpkgs {
           system = "x86_64-linux";
         };
 
-        modules = [ ./home.nix ];
+        modules = [
+		  ./home.nix
+		  catppuccin.homeModules.catppuccin
+		];
       };
   };
 }
--- home.nix
+++ home.nix
@@ -1,21 +1,41 @@
 { config, pkgs, lib, ... }:
 
+let
+  catppuccin-waybar = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "waybar";
+    rev = "main";
+    hash = "sha256-za0y6hcN2rvN6Xjf31xLRe4PP0YyHu2i454ZPjr+lWA";
+  };
+in
 {
   home.username = "sheb";
   home.homeDirectory = "/home/sheb";
   home.stateVersion = "26.05";
   
   programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    yazi
+    fastfetch
+  ];
+
+  catppuccin = {
+    enable = true;
+	autoEnable = true;
+	flavor = "mocha";
+  };
+
+  programs.btop.enable = true;
   
   programs.neovim = {
     enable = true;
   };
   home.file.".config/nvim".source = ./config/nvim;
   
-  home.packages = with pkgs; [
-    yazi
-    fastfetch
-  ];
+  home.file.".config/waybar".source = ./config/waybar;
+  home.file.".config/waybar/mocha.css".source =
+    "${catppuccin-waybar}/themes/mocha.css";
 
   home.file.".config/labwc".source = ./config/labwc;
 }
